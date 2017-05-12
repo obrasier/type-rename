0931b1bee550d27ab84b9a2534809fee8b926762
Owen Brasier <owen.brasier@sydney.edu.au>
2017-05-12 15:26:22 +1000

    replaces different data types with whitespace or newlines inside the delcaration

--- Example.cpp
+++ Example.cpp
@@ -9,8 +9,15 @@
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
 #include "clang/Rewrite/Core/Rewriter.h"
+#include "clang/Basic/TargetOptions.h"
+#include "clang/Basic/TargetInfo.h"
+#include "clang/Basic/FileManager.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/Lex/Preprocessor.h"
+#include "clang/Lex/Lexer.h"
 
 #include <iostream>
+#include <iterator>
 
 using namespace std;
 using namespace clang;
@@ -21,29 +28,43 @@ using namespace llvm;
 Rewriter rewriter;
 int numFunctions = 0;
 
+pair<string, string> type_replace[5] = {{"unsigned int", "uint16_t"}, {"unsigned long", "uint32_t"}, {"int", "int16_t"}, {"long", "int32_t"}, {"double", "float"}};
+
+// string decl_to_str(clang::Decl *d, )
+template<typename T>
+void replace_text(T input_decl, string r_text) {
+    auto in_start = input_decl->getLocStart();
+    auto in_end = input_decl->getLocEnd();
+    // gets the range of the total parameter - including type and argument
+    auto range = CharSourceRange::getTokenRange(in_start, in_end);
+    // get the stringPtr from the range and convert to string
+    string s = string(Lexer::getSourceText(range, rewriter.getSourceMgr(), rewriter.getLangOpts()));
+    // offset gives us the length of the argument, 1 is for the space
+    int offset = Lexer::MeasureTokenLength(in_end, rewriter.getSourceMgr(), rewriter.getLangOpts()) + 1;
+    // replace the text with the text sent
+    rewriter.ReplaceText(in_start, s.length() - offset, r_text);
+}
 
 class ExampleVisitor : public RecursiveASTVisitor<ExampleVisitor> {
-private:
+  private:
     ASTContext *astContext; // used for getting additional AST info
 
-public:
-    explicit ExampleVisitor(CompilerInstance *CI) 
-      : astContext(&(CI->getASTContext())) // initialize private members
-    {
+  public:
+    explicit ExampleVisitor(CompilerInstance *CI)
+        : astContext(&(CI->getASTContext())) { // initialize private members
         rewriter.setSourceMgr(astContext->getSourceManager(), astContext->getLangOpts());
     }
 
     virtual bool VisitFunctionDecl(FunctionDecl *func) {
         numFunctions++;
-        string funcName = func->getNameInfo().getName().getAsString();
         string retType = func->getReturnType().getAsString();
         string arg;
-        string int_type = "int";
+        string int_type = "unsigned int";
+        QualType arg2;
         int params = func->getNumParams();
-        if (funcName == "do_math") {
-            rewriter.ReplaceText(func->getLocation(), funcName.length(), "add5");
-            errs() << "** Rewrote function def: " << funcName << "\n";
-        }    
+        SourceLocation func_start = func->getLocStart();
+        SourceLocation param_start;
+        SourceLocation param_end;
         if (retType == "int") {
             rewriter.ReplaceText(func->getLocStart(), retType.length(), "int16_t");
             errs() << "** Rewrote function return: " << funcName << "\n";
@@ -51,10 +72,12 @@ public:
         if (params) {
             cout << "params in " << funcName << endl;
             for (int i = 0; i < params; ++i) {
-                arg = func->parameters()[i]->getType().getAsString();
-                // check if parameter begins with "int" -- covers int * type
-                if (arg.compare(0, int_type.length(), int_type) == 0) {
-                    rewriter.ReplaceText(func->parameters()[i]->getLocStart(), int_type.length(), "int16_t");
+                auto param = func->parameters()[i];
+                arg = param->getType().getAsString();
+                for (auto elem : type_replace) {
+                    if (arg == elem.first) {
+                        replace_text(param, elem.second);
+                    }
                 }
                 cout << arg << endl;
             }
@@ -67,7 +90,7 @@ public:
         if (ReturnStmt *ret = dyn_cast<ReturnStmt>(st)) {
             rewriter.ReplaceText(ret->getRetValue()->getLocStart(), 6, "val");
             errs() << "** Rewrote ReturnStmt\n";
-        }        
+        }
         if (CallExpr *call = dyn_cast<CallExpr>(st)) {
             rewriter.ReplaceText(call->getLocStart(), 7, "add5");
             errs() << "** Rewrote function call\n";
@@ -75,28 +98,28 @@ public:
         return true;
     }
 
-/*
-    virtual bool VisitReturnStmt(ReturnStmt *ret) {
-        rewriter.ReplaceText(ret->getRetValue()->getLocStart(), 6, "val");
-        errs() << "** Rewrote ReturnStmt\n";
-        return true;
-    }
+    /*
+        virtual bool VisitReturnStmt(ReturnStmt *ret) {
+            rewriter.ReplaceText(ret->getRetValue()->getLocStart(), 6, "val");
+            errs() << "** Rewrote ReturnStmt\n";
+            return true;
+        }
 
-    virtual bool VisitCallExpr(CallExpr *call) {
-        rewriter.ReplaceText(call->getLocStart(), 7, "add5");
-        errs() << "** Rewrote function call\n";
-        return true;
-    }
-*/
+        virtual bool VisitCallExpr(CallExpr *call) {
+            rewriter.ReplaceText(call->getLocStart(), 7, "add5");
+            errs() << "** Rewrote function call\n";
+            return true;
+        }
+    */
 };
 
 
 
 class ExampleASTConsumer : public ASTConsumer {
-private:
+  private:
     ExampleVisitor *visitor; // doesn't have to be private
 
-public:
+  public:
     // override the constructor in order to pass CI
     explicit ExampleASTConsumer(CompilerInstance *CI)
         : visitor(new ExampleVisitor(CI)) // initialize the visitor
@@ -109,34 +132,34 @@ public:
         visitor->TraverseDecl(Context.getTranslationUnitDecl());
     }
 
-/*
-    // override this to call our ExampleVisitor on each top-level Decl
-    virtual bool HandleTopLevelDecl(DeclGroupRef DG) {
-        // a DeclGroupRef may have multiple Decls, so we iterate through each one
-        for (DeclGroupRef::iterator i = DG.begin(), e = DG.end(); i != e; i++) {
-            Decl *D = *i;    
-            visitor->TraverseDecl(D); // recursively visit each AST node in Decl "D"
+    /*
+        // override this to call our ExampleVisitor on each top-level Decl
+        virtual bool HandleTopLevelDecl(DeclGroupRef DG) {
+            // a DeclGroupRef may have multiple Decls, so we iterate through each one
+            for (DeclGroupRef::iterator i = DG.begin(), e = DG.end(); i != e; i++) {
+                Decl *D = *i;
+                visitor->TraverseDecl(D); // recursively visit each AST node in Decl "D"
+            }
+            return true;
         }
-        return true;
-    }
-*/
+    */
 };
 
 
 
 class ExampleFrontendAction : public ASTFrontendAction {
-public:
+  public:
     virtual std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI, StringRef file) {
         return std::unique_ptr<ASTConsumer>(new ExampleASTConsumer(&CI)); // pass CI pointer to ASTConsumer
     }
 };
 
 
-static llvm::cl::OptionCategory MyToolCategory(""); 
+static llvm::cl::OptionCategory MyToolCategory("");
 
 int main(int argc, const char **argv) {
     // parse the command-line args passed to your code
-    CommonOptionsParser op(argc, argv, MyToolCategory);        
+    CommonOptionsParser op(argc, argv, MyToolCategory);
     // create a new Clang Tool instance (a LibTooling environment)
     ClangTool Tool(op.getCompilations(), op.getSourcePathList());
 
--- test.c
+++ test.c
@@ -1,9 +1,11 @@
-void do_math(int *x) {
-  *x += 5;
+void do_math(int x, unsigned
+int  lolol) {
+  x += 5;
 }
+
 
 int main(void) {
   int result = -1, val = 4;
-  do_math(&val);
+  do_math(val, result);
   return result;
 }
